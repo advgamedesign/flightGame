631090954446a6bebcae9a45e0be7a9446b5d311
Jacob Greene <jgree178@uncc.edu>
2019-02-22 23:15:36 -0500

    PlayerMovement Script

---
+++ ArcadeFlightGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("PlaneMovement Script Added To: " + gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, Input.GetAxis("Horizontal"));
+    }
+}
