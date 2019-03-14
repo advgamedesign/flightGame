a84a797b7a089eb90f8834616907204bb1946aea
Jacob Greene <jgree178@uncc.edu>
2019-03-13 20:49:09 -0400

    Updated Arrow Key Movement

--- ArcadeFlightGame/Assets/Scripts/PlayerMovement.cs
+++ ArcadeFlightGame/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float speed = 40.0f;
+    //Initial Forward Velocity of Plane
+    public float velocity = 40.0f;
+    //Speed of Arrow Key movement
+    public float directionalSpeed = 10.0f;
+
+
     // Start is called before the first frame update
     void Start() {
         Debug.Log("PlaneMovement Script Added To: " + gameObject.name);
@@ -12,27 +17,37 @@ public class PlayerMovement : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        Vector3 moveCamTo = transform.position - transform.forward * 50.0f + Vector3.up * 5.0f;
-        Camera.main.transform.position = moveCamTo;
-        Camera.main.transform.LookAt(transform.position + transform.forward * 30.0f);
 
+        //Set Initial Forward Motion
+        transform.Translate(0, 0, 1 * velocity * Time.deltaTime);
 
+        #region Arrow Movement
+        //------------Setup Initial Arrow Flight Movement---------------
 
-        transform.position += transform.forward * Time.deltaTime * speed;
-
-        speed -= transform.forward.y * Time.deltaTime * 50.0f;
-
-        if(speed < 10.0f) {
-            speed = 10.0f;
+        //When Up Arrow key is pushed down
+        if(Input.GetKey(KeyCode.UpArrow)) {
+            //Transform Position Z to Move Player Up
+            transform.Translate(0, 1 * directionalSpeed * Time.deltaTime, 0);
         }
 
-
-        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -2 * Input.GetAxis("Horizontal"));
-
-        float terrainHeightAtPosition = Terrain.activeTerrain.SampleHeight(transform.position);
-
-        if(terrainHeightAtPosition > transform.position.y) {
-            transform.position = new Vector3(transform.position.x, terrainHeightAtPosition, transform.position.z);
+        //When Down Arrow key is pushed down
+        if(Input.GetKey(KeyCode.DownArrow)) {
+            //Transform Position Z to Move Player Down
+            transform.Translate(0, 1 * -directionalSpeed * Time.deltaTime, 0);
         }
+
+        //When Right Arrow key is pushed down
+        if(Input.GetKey(KeyCode.RightArrow)) {
+            //Transform Position Z to Move Player Right
+            transform.Translate(1 * directionalSpeed * Time.deltaTime, 0, 0);
+        }
+
+        //When Left Arrow Key is pushed down
+        if(Input.GetKey(KeyCode.LeftArrow)) {
+            //Transform Position Z to Move Player Left
+            transform.Translate(1 * -directionalSpeed * Time.deltaTime, 0, 0);
+        }
+
+        #endregion
     }
 }
