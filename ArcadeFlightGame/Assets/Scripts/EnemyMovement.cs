200e59cd7c6f6f605efd9dfc721f73dea55aae27
greenej10 <jacobgreene.rfp@gmail.com>
2019-04-25 01:27:42 -0400

    scripts

---
+++ ArcadeFlightGame/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform[] points;
+    public float speed;
+
+    private int current;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != points[current].position)
+        {
+
+            Vector3 pos = Vector3.MoveTowards(transform.position, points[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+
+        }
+
+        else
+        {
+            current = (current + 1) % points.Length;
+        }
+    }
+}
