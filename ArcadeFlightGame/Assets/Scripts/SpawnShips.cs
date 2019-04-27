ca11f352646aecb2e1764cef19132cb9c3d6a9b6
greenej10 <jacobgreene.rfp@gmail.com>
2019-04-27 03:09:58 -0400

    spawning

---
+++ ArcadeFlightGame/Assets/Scripts/SpawnShips.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnShips : MonoBehaviour
+{
+    public GameObject ship;
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+    public int ships;
+    public int spawned;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnNewShip", spawnTime, spawnDelay);
+    }
+
+    // Update is called once per frame
+    public void SpawnNewShip()
+    {
+        if (spawned <= ships)
+        {
+            Instantiate(ship, transform.position, transform.rotation);
+            spawned++;
+        }
+        else
+        {
+            CancelInvoke("SpawnNewShip");
+        }
+    }
+}
