7973f12c6f50d7caf66003a645f8e06963c08062
greenej10 <jacobgreene.rfp@gmail.com>
2019-04-29 23:23:54 -0400

    collision

---
+++ ArcadeFlightGame/Assets/Scripts/IgnorePlayerBullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnorePlayerBullet : MonoBehaviour
+{
+
+    public GameObject bullet;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if(collision.collider.gameObject.tag == "playerBullet")
+        Physics.IgnoreCollision(collision.collider, gameObject.GetComponent<Collider>());
+    }
+
+}
